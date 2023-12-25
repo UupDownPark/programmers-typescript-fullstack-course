b0b99831fb1571902e7ca5e9849fa09ecb08fde1
UupDownPark <geakura123@gmail.com>
2023-12-26 01:26:29 +0900

    express practice

--- express/youtuber-demo.js
+++ express/youtuber-demo.js
@@ -1,33 +1,41 @@
 const express = require("express");
 const res = require("express/lib/response");
 const app = express();
-const port = 1235;
+const port = 1234;
 
 let youtuber1 = {
-  channelName: "십오야",
+  channelTitle: "십오야",
   sub: "12만",
   videoNum: "991개",
 };
 
 let youtuber2 = {
-  channelName: "상하tv",
+  channelTitle: "상하tv",
   sub: "120만",
   videoNum: "300개",
 };
 
 let youtuber3 = {
-  channelName: "상하먹방",
+  channelTitle: "상하먹방",
   sub: "511만",
   videoNum: "777개",
 };
 
 let db = new Map();
-db.set(1, youtuber1);
-db.set(2, youtuber2);
-db.set(3, youtuber3);
+let id = 1;
+db.set(id++, youtuber1);
+db.set(id++, youtuber2);
+db.set(id++, youtuber3);
 
 app.use(express.json()); //http 모듈외에 미들웨어 설정:json설정
-app.get("/youtuber/:id", (req, res) => {
+app.get("/youtubers", function (req, res) {
+  let youtubers = {};
+  db.forEach((value, key) => {
+    youtubers[key] = value;
+  });
+  res.json(youtubers);
+});
+app.get("/youtubers/:id", (req, res) => {
   let { id } = req.params;
   id = parseInt(id);
   // youtuber가 없으면
@@ -40,12 +48,61 @@ app.get("/youtuber/:id", (req, res) => {
   }
 });
 
-app.post("/youtuber", (req, res) => {
+app.post("/youtubers", (req, res) => {
   console.log(req.body);
-  db.set(4, req.body);
-  res.json(req.body);
+  db.set(id++, req.body);
+  res.json({
+    message: `${db.get(id - 1).channelTitle} 유투버가 되신것을 환영합니다.`,
+  });
+});
+app.delete("/youtubers", (req, res) => {
+  let msg = "";
+  if (db.size === 0) {
+    msg = "삭제할 유투버가 없습니다.";
+  } else {
+    db.clear();
+    msg = "전체 유투버가 삭제되었습니다.";
+  }
+  res.json({
+    message: msg,
+  });
 });
 
+app.delete("/youtubers/:id", (req, res) => {
+  let { id } = req.params;
+  id = parseInt(id);
+  var youtuber = db.get(id);
+  if (youtuber == undefined) {
+    res.json({
+      message: "요청하신 유투버는 가입된 유투버가 아닙니다.",
+    });
+  } else {
+    let deleter = youtuber.channelTitle;
+    db.delete(id);
+    res.json({
+      message: `${deleter}님 , 아쉽지만 우리 인연은 여기까지 인가요...`,
+    });
+  }
+});
+
+app.put("/youtubers/:id", (req, res) => {
+  let { id } = req.params;
+  id = parseInt(id);
+  var youtuber = db.get(id);
+  var oldTitle = youtuber.channelTitle;
+  if (youtuber == undefined) {
+    res.json({
+      message: "요청하신 유투버는 가입된 유투버가 아닙니다.",
+    });
+  } else {
+    let newTitle = req.body.channelTitle;
+    youtuber.channelTitle = newTitle;
+    db.set(id, youtuber);
+    res.json({
+      message: `${oldTitle}이 ${newTitle}로 수정되었습니다.`,
+    });
+  }
+});
 app.listen(port, () => {
   console.log(`good working on port ${port}`);
 });
